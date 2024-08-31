a27f8ed91897ca6996be94218ec49c625c9ebaf2
Ayush Shrivastava <ayush.shrivastava.023@gmail.com>
2024-08-31 15:38:34 +0530

    Event Loop Demonstration

--- Async_JavaScript/AsyncJS.js
+++ Async_JavaScript/AsyncJS.js
@@ -5,37 +5,37 @@ const countriesContainer = document.querySelector(".countries");
 
 //----------------------------------------------------------------------------------------------------------------------------------------
 
-const renderCountry = function (data, className = "") {
-  const html = `
-    <article class="country ${className}">
-      <img class="country__img" src="${data.flags.png}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name.common}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)} people</p>
-        <p class="country__row"><span>🗣️</span>${data.continents[0]}</p>
-        <p class="country__row"><span>💰</span>${data.capital[0]}</p>
-      </div>
-    </article>
-    `;
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-};
+// const renderCountry = function (data, className = "") {
+//   const html = `
+//     <article class="country ${className}">
+//       <img class="country__img" src="${data.flags.png}" />
+//       <div class="country__data">
+//         <h3 class="country__name">${data.name.common}</h3>
+//         <h4 class="country__region">${data.region}</h4>
+//         <p class="country__row"><span>👫</span>${(
+//           +data.population / 1000000
+//         ).toFixed(1)} people</p>
+//         <p class="country__row"><span>🗣️</span>${data.continents[0]}</p>
+//         <p class="country__row"><span>💰</span>${data.capital[0]}</p>
+//       </div>
+//     </article>
+//     `;
+//   countriesContainer.insertAdjacentHTML("beforeend", html);
+//   countriesContainer.style.opacity = 1;
+// };
 
-const renderError = function (msg) {
-    countriesContainer.insertAdjacentText('beforeend', msg);
-    countriesContainer.style.opacity = 1;
-};
+// const renderError = function (msg) {
+//     countriesContainer.insertAdjacentText('beforeend', msg);
+//     countriesContainer.style.opacity = 1;
+// };
 
-const getJSON =  function (url, errorMsg = 'Something went wrong') {
-  return fetch(url).then(response => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+// const getJSON =  function (url, errorMsg = 'Something went wrong') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
 
-    return response.json();
-  });
-};
+//     return response.json();
+//   });
+// };
 
 //----------------------------------------------------------------------------------------------------------------------------------------
 
@@ -81,39 +81,69 @@ const getJSON =  function (url, errorMsg = 'Something went wrong') {
 
 // getCountryData("portugal");
 
-
 // Throwing errors manually
-const getCountryData = function (country) {
-  // Country 1
-  getJSON(
-    `https://restcountries.com/v3.1/name/${country}`,
-    'Country not found'
-  )
-    .then(data => {
-      renderCountry(data[0]);
-      const neighbour = data[0].borders?.[0];
+// const getCountryData = function (country) {
+//   // Country 1
+//   getJSON(
+//     `https://restcountries.com/v3.1/name/${country}`,
+//     'Country not found'
+//   )
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders?.[0];
 
-      if (!neighbour) throw new Error('No neighbour found!');
+//       if (!neighbour) throw new Error('No neighbour found!');
 
-      // Country 2
-      return getJSON(
-        `https://restcountries.com/v3.1/name/${neighbour}`,
-        'Country not found'
-      );
-    })
+//       // Country 2
+//       return getJSON(
+//         `https://restcountries.com/v3.1/name/${neighbour}`,
+//         'Country not found'
+//       );
+//     })
 
-    .then(data => renderCountry(data, 'neighbour'))
-    .catch(err => {
-      console.error(`${err} 💥💥💥`);
-      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
-};
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
 
 // btn.addEventListener('click', function () {
 //   getCountryData('portugal');
 // });
 
-getCountryData('australia');
+// getCountryData('australia');
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+// Event loop, callback queue and micro-task queue
+
+// The event loop puts the tasks from queue to call stack when the call stack is empty or we can say when it only contains global context.
+
+// Micro-task queue only takes promises callbacks and have a higher priority over callback queue, which means it will first finish the micro-tasks queue tasks and then execute the callback queue tasks.  [VVV imp]
+
+console.log("Starting");
+
+// The callback of the timer will go the callback queue and will be executed after all the micro-tasks will be executed.
+setTimeout(() => {
+  console.log("0 sec timer");
+}, 0);
+
+// The callback of the promise will go in micro-task queue and will be executed first
+Promise.resolve().then(() => {
+  console.log("Resolved Promise at the same time as timer");
+});
+
+// If the promise's callback takes a lot of time to get executed then the timer will have to wait for more than the specified time which is a disadvantage and a good interview question.
+Promise.resolve("Resolved promise").then((res) => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log("End");
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
