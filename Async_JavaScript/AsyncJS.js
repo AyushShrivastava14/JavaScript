ed46038d10ffdb99f0397fd1a0c91501ed568c43
Ayush Shrivastava <ayush.shrivastava.023@gmail.com>
2024-08-31 18:50:45 +0530

    async/await with try-catch

--- Async_JavaScript/AsyncJS.js
+++ Async_JavaScript/AsyncJS.js
@@ -148,44 +148,64 @@ const countriesContainer = document.querySelector(".countries");
 //----------------------------------------------------------------------------------------------------------------------------------------
 
 // Building a Simple Promise
-const lotteryPromise = new Promise(function (resolve, reject) {
-  console.log("Lotter draw is happening 🔮");
-  setTimeout(function () {
-    if (Math.random() >= 0.5) {
-      resolve("You WIN 💰");
-    } else {
-      reject(new Error("You lost your money 💩"));
-    }
-  }, 2000);
-});
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log("Lotter draw is happening 🔮");
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve("You WIN 💰");
+//     } else {
+//       reject(new Error("You lost your money 💩"));
+//     }
+//   }, 2000);
+// });
 
-lotteryPromise
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+// lotteryPromise
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
 
-// Promisifying setTimeout
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
-  });
+// // Promisifying setTimeout
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(1)
+//   .then(() => {
+//     console.log("1 second passed");
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log("2 second passed");
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log("3 second passed");
+//     return wait(1);
+//   })
+//   .then(() => console.log("4 second passed"));
+
+// Promise.resolve("abc").then((x) => console.log(x));
+// Promise.reject(new Error("Problem!")).catch((x) => console.error(x));
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+// Consuming Promises with Async/Await
+// Error Handling With try...catch
+
+const whereAmI = async function (country) {
+  try {
+    const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+
+    const data = await res.json();
+    console.log(data[0]);
+  } catch (err) {
+    console.error(`${err} 💥`);
+    renderError(`💥 ${err.message}`);
+  }
 };
 
-wait(1)
-  .then(() => {
-    console.log("1 second passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("2 second passed");
-    return wait(1);
-  })
-  .then(() => {
-    console.log("3 second passed");
-    return wait(1);
-  })
-  .then(() => console.log("4 second passed"));
-
-Promise.resolve("abc").then((x) => console.log(x));
-Promise.reject(new Error("Problem!")).catch((x) => console.error(x));
+whereAmI('portugal');
+console.log('FIRST');
 
 //----------------------------------------------------------------------------------------------------------------------------------------
